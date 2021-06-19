e41dd9c6a4914be75d4297795a75fc5d4c6d177c
root1 <shubhomj@geniusplaza.com>
2021-06-19 21:14:22 +0530

    data fetching with json

--- Json/tinkerwithjson.js
+++ Json/tinkerwithjson.js
@@ -2,3 +2,4 @@ const fs = require("fs");
 const buffer = fs.readFileSync("./example.json");
 const data = JSON.parse(buffer);
 console.log(data);
+ 
--- NodeModules/Chalk.js
+++ NodeModules/Chalk.js
@@ -6,3 +6,4 @@ const chalk = require("chalk");
 
 const figlet = require("figlet");
 console.log(chalk.red(figlet.textSync("Rahul")));
+ 
